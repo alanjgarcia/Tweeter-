ed41f7690197542b1191e8dc618c26722d161abc
Alan Garcia <alangarciacoding@gmail.com>
2022-09-18 16:14:59 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-User.create(name:"Jeremy", email: "dimes@test.com", password: "password")
+User.create(name:"Jeremy", email: "dimes@test.com", password: "password", image: "gsdanf")
 
-User.create(name: "Gita", email: "gita@test.com", password: "cat")
+User.create(name: "Gita", email: "gita@test.com", password: "cat", image: "sgasdg")
 
+User.create(name: "Alan", email: "alan@test.com", password: "dog", image: "dsafd")
